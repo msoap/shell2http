80e1cf64bc473acc25cbcceace4c3cb02dbe0e89
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-03-11 23:07:20 +0200

    Changes from golint

--- shell2http.go
+++ shell2http.go
@@ -1,22 +1,22 @@
 /*
-	Execute shell commands via http server
+Execute shell commands via http server
 
-	Install:
-		go get github.com/msoap/shell2http
-		ln -s $GOPATH/bin/shell2http ~/bin/shell2http
+Install:
+	go get github.com/msoap/shell2http
+	ln -s $GOPATH/bin/shell2http ~/bin/shell2http
 
-	Usage:
-		shell2http /path "shell command" /path2 "shell command2" ...
+Usage:
+	shell2http /path "shell command" /path2 "shell command2" ...
 
-	Examples:
-		shell2http /top "top -l 1 | head -10"
-		shell2http /date date /ps "ps aux"
-		shell2http /env 'printenv | sort' /env/path 'echo $PATH' /env/gopath 'echo $GOPATH'
-		shell2http /shell_vars_json 'perl -MJSON -E "say to_json(\%ENV)"'
-		shell2http /cal_html 'echo "<html><body><h1>Calendar</h1>Date: <b>$(date)</b><br><pre>$(cal $(date +%Y))</pre></body></html>"'
+Examples:
+	shell2http /top "top -l 1 | head -10"
+	shell2http /date date /ps "ps aux"
+	shell2http /env 'printenv | sort' /env/path 'echo $PATH' /env/gopath 'echo $GOPATH'
+	shell2http /shell_vars_json 'perl -MJSON -E "say to_json(\%ENV)"'
+	shell2http /cal_html 'echo "<html><body><h1>Calendar</h1>Date: <b>$(date)</b><br><pre>$(cal $(date +%Y))</pre></body></html>"'
 
-	Update:
-		go get -u github.com/msoap/shell2http
+Update:
+	go get -u github.com/msoap/shell2http
 
 */
 package main
@@ -29,6 +29,7 @@ import (
 	"os/exec"
 )
 
+// default port for http-server
 const PORT = "8080"
 
 // ------------------------------------------------------------------
