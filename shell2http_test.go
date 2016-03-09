d1f6cccbe4f950d5fa6a67bf6f9a61210b7d5506
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-03-10 00:25:53 +0200

    First version of test main

--- shell2http_test.go
+++ shell2http_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -70,5 +75,60 @@ func Test_getShellAndParams(t *testing.T) {
 	shell, params, err = getShellAndParams("ls '-l", "", false)
 	if err == nil {
 		t.Errorf("6. getShellAndParams() failed")
+	}
+}
+
+func httpGet(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func getFreePort() string {
+	listen, _ := net.Listen("tcp", ":0")
+	defer listen.Close()
+	parts := strings.Split(listen.Addr().String(), ":")
+
+	return parts[len(parts)-1]
+}
+
+func Test_main1(t *testing.T) {
+	port := getFreePort()
+	os.Args = []string{"shell2http",
+		"-add-exit",
+		"-cache=1",
+		"-cgi",
+		"-export-all-vars",
+		"-form",
+		"-one-thread",
+		"-shell=bash",
+		"-log=/dev/null",
+		"-port=" + port,
+		"/echo", "echo 123"}
+	go main()
+
+	res, err := httpGet("http://localhost:" + port + "/")
+	if err != nil {
+		t.Errorf("1. main() failed: %s", err)
+	}
+	if len(res) == 0 || !strings.HasPrefix(string(res), "<!DOCTYPE html>") {
+		t.Errorf("1. main() failed: real result: '%s'", string(res))
+	}
+
+	res, err = httpGet("http://localhost:" + port + "/echo")
+	if err != nil {
+		t.Errorf("2. main() failed: %s", err)
+	}
+	if string(res) != "123\n" {
+		t.Errorf("2. main() failed: real result: '%s'", string(res))
 	}
 }
